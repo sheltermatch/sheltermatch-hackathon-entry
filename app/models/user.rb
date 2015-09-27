b70287f637b4b18c01d04eb1e1bc8677cca10169
Cody Roberts <codyjroberts@gmail.com>
2015-09-26 19:55:28 -0500

    update devise:confirmable and routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ActiveRecord::Base
   rolify
+  belongs_to :shelter
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-
-  belongs_to :shelter
+    :recoverable, :rememberable, :trackable, :validatable, :confirmable
 end
