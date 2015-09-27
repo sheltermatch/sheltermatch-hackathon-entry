1bab1031cb9758c25cc604c95f96eb1be030d47e
Cody Roberts <codyjroberts@gmail.com>
2015-09-26 21:13:00 -0500

    added shelter routes, fixed controllers

--- app/controllers/shelter_controller.rb
+++ app/controllers/shelter_controller.rb
@@ -1,5 +1,27 @@
-class ShelterController < ApplicationController
+class SheltersController < ApplicationController
+  before_action :authenticate_user!
+
   def index
 
+  end
+
+  def new
+    @shelter = Shelter.new
+  end
+
+  def create
+    @shelter = Shelter.create(shelter_params)
+
+    if @shelter.save
+      redirect_to shelter_path, notice: "Created shelter"
+    else
+      render :new, notice: "Error creating new shelter"
+    end
+  end
+
+  private
+
+  def shelter_params
+    params.require(:shelter).permit(:user)
   end
 end
